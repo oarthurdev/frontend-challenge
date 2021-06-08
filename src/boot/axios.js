fb490e283e8ba29889dc1d431bf0ceed3e066a86
Arthur Wagenknecht <oarthurdev@gmail.com>
2021-06-08 18:59:47 -0300

    First Commit.

---
+++ src/boot/axios.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import httpAdapter from 'axios/lib/adapters/http'
+
+axios.defaults.baseURL = 'http://localhost:8080'
+
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+axios.defaults.headers.common['Content-Type'] = 'application/json'
+axios.defaults.headers.common['Accept'] = 'application/json'
+
+axios.defaults.adapter = httpAdapter
+
+module.exports = axios
